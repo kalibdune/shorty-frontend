185f22b928e25be4f8e709f19832c8616ddb6902
dmitry <kalibdune@yandex.ru>
2025-03-21 23:16:22 +0300

    initial structure

---
+++ webpack/webpack.common.js
@@ -0,0 +1,83 @@
+const path = require('path');
+const HTMLWebpackPlugins = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
+
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = {
+    entry: path.resolve(__dirname, '../src/index.ts'),
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'main.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.[tj]sx?$/,
+                use: [
+                    {
+                        loader: 'ts-loader',
+                    },
+                ],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                mode: 'local',
+                                localIdentName: '[name]__[local]__[hash:base64:5]',
+                                auto: /\.module\.\w+$/i,
+                            },
+                            importLoaders: 2,
+                        },
+                    },
+                    'postcss-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpg|gif|webp)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/images/[hash][ext][query]',
+                },
+            },
+            {
+                test: /\.(woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'static/fonts/[hash][ext][query]',
+                },
+            },
+            {
+                test: /\.svg$/i,
+                issuer: /\.[jt]sx?$/,
+                use: ['@svgr/webpack', 'url-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HTMLWebpackPlugins({
+            template: path.resolve(__dirname, '../public/index.html')
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'static/styles/index.css',
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.tsx', '.ts', '.json'],
+    },
+};
