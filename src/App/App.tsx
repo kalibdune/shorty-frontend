8c6726e07f103a40fa92ebb7a3ad271508ab1274
dmitry <kalibdune@yandex.ru>
2025-05-21 21:28:27 +0300

    rename App

---
+++ src/App/App.tsx
@@ -0,0 +1,23 @@
+import Main from '../pages/Main/Main'
+import { Auth } from '../pages/Auth/Auth'
+import Background from '../components/Background/Background'
+import { Routes, Route } from 'react-router-dom'
+import '../index.scss'
+import Urls from '../pages/Urls/Urls'
+import Profile from '../pages/Profile/Profile'
+
+const App: React.FC = () => {
+	return (
+		<>
+			<Background></Background>
+			<Routes>
+				<Route path='/' element={<Main />} />
+				<Route path='/auth' element={<Auth />} />
+				<Route path='/urls' element={<Urls />} />
+				<Route path='/profile' element={<Profile />} />
+			</Routes>
+		</>
+	)
+}
+
+export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-import App from './app/app'
+import App from './App/App'
 
 const domNode = document.getElementById('root') as HTMLDivElement
 const root = createRoot(domNode)
